24844cf2296e7dfaf8cece30daff724f9775d7a2
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-01-15 18:37:22 +0530

    issue resolved

--- Minikube/minikube-install-CentOS.sh
+++ Minikube/minikube-install-CentOS.sh
@@ -13,33 +13,31 @@
 echo -n 'Do you want to start Minikube Now? (Y/N)'
 read VAR
 
-echo $VAR
-
-# while [[ $VAR != 'Y' ]] && [[ $VAR != 'y' ]] && [[ $VAR = 'N' ]] && [[ $VAR = 'n' ]]
-# do
-    if [[ $VAR = 'Y' ]]
+while [[ $VAR != 'Y' ]] || [[ $VAR != 'y' ]] || [[ $VAR = 'N' ]] || [[ $VAR = 'n' ]]
+do
+    if [[ $VAR = 'Y' ]] || [[ $VAR = 'y' ]]
     then
         minikube start
-    elif [[ $VAR = 'N' ]] && [[ $VAR = 'n' ]]
+    elif [[ $VAR = 'N' ]] || [[ $VAR = 'n' ]]
     then
         echo 'Use minikube start command to run minikube manually'
     else 
         echo -n 'Do you want to start Minikube Now? (Y/N)'
         read VAR
     fi
-# done
+done
 
 # #Add minikube kubectl to alias
 # echo -n 'Do you want to add minikube kubectl to run easiar with only kubectl command? (Y/N)'
 # read VARR
 
-# while [[ $VARR != 'Y' ]] && [[ $VARR != 'y' ]] && [[ $VARR = 'N' ]] && [[ $VARR = 'n' ]]
+# while [[ $VARR != 'Y' ]] || [[ $VARR != 'y' ]] || [[ $VARR = 'N' ]] || [[ $VARR = 'n' ]]
 # do
-#     if [[ $VARR = 'Y' ]] && [[ $VARR = 'y' ]]
+#     if [[ $VARR = 'Y' ]] || [[ $VARR = 'y' ]]
 #     then
 #         alias kubectl="minikube kubectl --"
 #         echo 'You can run minikube kubectl using only - kubectl ...'
-#     elif [[ $VARR = 'N' ]] && [[ $VARR = 'n' ]]
+#     elif [[ $VARR = 'N' ]] || [[ $VARR = 'n' ]]
 #     then
 #         echo 'You have to run kubectl using this command - minikube kubectl ...'
 #     else 
