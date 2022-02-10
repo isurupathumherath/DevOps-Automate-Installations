effd81553226bec3476646bd92a32e430739be83
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2022-02-10 16:02:17 +0530

    updated

--- Docker + Jenkins/install.sh
+++ Docker + Jenkins/install.sh
@@ -27,7 +27,7 @@ usermod -aG docker isurupathum  # NOTE - Change isurupathum to the name of your 
 
 # Install Jenkins
 sudo docker build -t jenkins-docker .
-sudo docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:var/run/docker.sock --name jenkins -d jenkins
+sudo docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:var/run/docker.sock --name jenkins -d jenkins-docker
 
 # show endpoint
 echo
